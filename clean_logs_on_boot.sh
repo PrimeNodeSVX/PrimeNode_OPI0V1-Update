8f07cfd2aafc6dc21f66af59a8e49e7213f9f02e
ArduUTP <sq7utp.git@gmail.com>
2026-02-03 01:01:40 +0100

    Update clean_logs_on_boot.sh

--- clean_logs_on_boot.sh
+++ clean_logs_on_boot.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+
+LOG_RAM="/dev/shm/svxlink.log"
 ARCHIVE_DIR="/root/svxlink_history"
-MAX_ARCHIVES=5
 FLAG_ONLINE="/var/www/html/el_online.flag"
 FLAG_ERROR="/var/www/html/el_error.flag"
-LOG_RAM="/dev/shm/svxlink.log"
 
 systemctl stop svxlink
 rm -f "$FLAG_ONLINE" "$FLAG_ERROR"
+rm -f "$LOG_RAM"
 touch "$LOG_RAM"
-chmod 666 "$LOG_RAM"
+chmod 777 "$LOG_RAM"
 pkill -9 -f "svx_event_logger.sh"
 nohup /usr/local/bin/svx_event_logger.sh > /dev/null 2>&1 &
-sleep 2
-/usr/bin/python3 /usr/local/bin/update_svx_full.py
+sed -i 's|^LOGFILE=.*|LOGFILE=/dev/shm/svxlink.log|g' /etc/svxlink/svxlink.conf
 
+sleep 2
 systemctl start svxlink
 
 exit 0
